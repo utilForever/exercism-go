2211e40c56321605b740693f3700963e3666b6a2
Chris Ohk <utilforever@gmail.com>
2022-03-06 23:09:04 +0900

    Upload exercise "Gopher's Gorgeous Lasagna"

---
+++ lasagna.go
@@ -0,0 +1,16 @@
+package lasagna
+
+const OvenTime = 40
+
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+func PreparationTime(numberOfLayers int) int {
+	return 2 * numberOfLayers
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
+}
